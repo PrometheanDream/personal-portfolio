2807eadd7f3b7c4644867b0f7d382d48c1ea5322
William Simpson <simpswc3911@gmail.com>
2017-12-03 20:58:15 -0600

    adding simpson-typesValuesVariables.js

---
+++ week-1/simpson-typesValuesVariables.js
@@ -0,0 +1,114 @@
+/*
+============================================
+; Title:  Assignment 1.5
+; Author: Albert Einstein
+; Date:   1 December 2017
+; Modified By: William Simpson
+; Description: This program demonstrates the
+;   use of JavaScript types, values, and 
+;   and variables in an application.
+;===========================================
+*/ 
+
+/*
+    The code below builds a properly formatted
+    header and must be included in all applications
+    you write. In subsequent week's we will build this
+    functionality into a function and place it in a separate file.
+*/
+
+var myFirstName = "William";
+var myLastName = "Simpson";
+var todaysDate = new Date().toLocaleDateString();
+var assignmentNum = "Assignment 1.5";
+
+var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
+    + assignmentNum + "\nDate: " + todaysDate;
+
+console.log(programHeader);
+console.log("\n"); 
+
+// start of program - your code goes below this line
+var firstName = "First Name: Billy"; 
+var lastName =  "Last Name: Joel";
+var middleName = "Middle Name: Bob";
+var address = "Address: 7112 Turing Road Boston, MA";
+var payRate = "Pay Rate: $" + 10.5;
+var hireDate = "Hire Date: 6/21/2017"
+
+console.log(firstName);
+console.log(lastName);
+console.log(middleName);
+console.log(address);
+console.log(payRate);
+console.log(hireDate);
+console.log("\n"); 
+
+
+
+var firstName = "First Name: Reggie"; 
+var lastName =  "Last Name: Sampson";
+var middleName = "Middle Name: J";
+var address = "Address: 11232 Advent St New York, NY";
+var payRate = "Pay Rate: $" + 100.8;
+var hireDate = "Hire Date: 3/10/2000"
+
+console.log(firstName);
+console.log(lastName);
+console.log(middleName);
+console.log(address);
+console.log(payRate);
+console.log(hireDate);
+console.log("\n");
+
+var firstName = "First Name: Aggie"; 
+var lastName =  "Last Name: Johaanas";
+var middleName = "Middle Name: Calia";
+var address = "Address: 3432 Courrier Court Fredericks Berg, AZ";
+var payRate = "Pay Rate: $" + 45.3;
+var hireDate = "Hire Date: 5/17/1999"
+
+console.log(firstName);
+console.log(lastName);
+console.log(middleName);
+console.log(address);
+console.log(payRate);
+console.log(hireDate);
+console.log("\n"); 
+
+var firstName = "First Name: Gertrude"; 
+var lastName =  "Last Name: McGuff";
+var middleName = "Middle Name: None";
+var address = "Address: 1717 Fieldsbrook Lane Sharpton, IA";
+var payRate = "Pay Rate: $" + 33.5;
+var hireDate = "Hire Date: 2/21/2005"
+
+console.log(firstName);
+console.log(lastName);
+console.log(middleName);
+console.log(address);
+console.log(payRate);
+console.log(hireDate);
+console.log("\n"); 
+
+var firstName = "First Name: Michael"; 
+var lastName =  "Last Name: Pichael";
+var middleName = "Middle Name: Twin";
+var address = "Address: 1127 Outerspace Drive Colorado Springs, CO";
+var payRate = "Pay Rate: $" + 20.15;
+var hireDate = "Hire Date: 11/21/2008"
+
+console.log(firstName);
+console.log(lastName);
+console.log(middleName);
+console.log(address);
+console.log(payRate);
+console.log(hireDate);
+console.log("\n"); 
+
+// end of program 
+
+
+
+
+ 
