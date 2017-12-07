4d313decf3c387aab239bf94ad25e1d4a21b800f
William Simpson <simpswc3911@gmail.com>
2017-12-06 19:21:01 -0600

    adding header to base directory

---
+++ simpson-header.js
@@ -0,0 +1,8 @@
+
+
+var header = require('../header.js');
+
+console.log("\nTesting the header.js file's output..."); 
+
+console.log(header.display("Simpson", "William", "Exercise 2.2")); 
+console.log("\n");
