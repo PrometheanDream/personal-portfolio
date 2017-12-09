68c7114837c6948627547d1dba4a9929761771ca
William Simpson <simpswc3911@gmail.com>
2017-12-09 10:46:19 -0600

    adding assignment 2.2 and 2.3

--- week-2/simpson-header.js
+++ week-2/simpson-header.js
@@ -4,5 +4,5 @@ var header = require('../header.js');
 
 console.log("\nTesting the header.js file's output..."); 
 
-console.log(header.display("Simpson", "William", "Exercise 2.2")); 
+console.log(header.display("William", "Simpson", "Exercise 2.2")); 
 console.log("\n");
